6b86da54affc0527c8c57508fbc1bb5207a59989
int-arsh <jainaakash303@gmail.com>
2025-10-04 23:56:09 +0530

    Initialize Next.js project with TypeScript, Tailwind CSS, and basic structure. Added .gitignore, package.json, and essential configuration files. Created initial layout and components including Navbar and profile page.

---
+++ src/app/layout.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar";
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html>
+			<body>
+                <Navbar />
+                {children}
+            </body>
+		</html>
+	);
+}
