68c369db21896f4f25b4e129411a64b13ed7e8ff
int-arsh <jainaakash303@gmail.com>
2025-10-05 19:45:40 +0530

    Refactor layout and Navbar components. Added global styles and improved Navbar with responsive design and links. Removed unused ProfilePage component.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
 import Navbar from "@/components/Navbar";
+import "./globals.css";
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html>
 			<body>
-                <Navbar />
-                {children}
+				<div className="w-full bg-white px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+					<Navbar />
+				</div>
+				<div className="bg-slate-100 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+                	{children}
+				</div>
             </body>
 		</html>
 	);
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,59 @@
+import Link from "next/link";
+import MobileMenu from "./MobileMenu";
+import Image from "next/image";
+
 export default function Navbar() {
-	return <div>Navbar</div>;
+	return (
+		<div className="h-24 flex items-center justify-between">
+			{/* left */}
+			<div className="md:hidden lg:block w-[20%]"> 
+				<Link href="/" className="font-bold text-xl text-blue-600"> Echo </Link> 
+			</div>
+
+			{/* center */}
+			<div className="hidden md:flex w-[50%] text-sm items-center justify-between"> 
+				{/* Links*/}
+                <div className="flex gap-6 text-gray-600">
+                    <Link href="/" className="flex items-center gap-2">
+						<Image
+							src="/home.png"
+							alt="Homepage"
+							width={16}
+							height={16}
+							className="w-4 h-4"
+						
+						/>
+						<span>Homepage</span>
+                    </Link>
+                    <Link href="/" className="flex items-center gap-2">
+						<Image
+							src="/friends.png"
+							alt="Friends"
+							width={16}
+							height={16}
+							className="w-4 h-4"
+						/>
+						<span>Friends</span>
+                    </Link>
+                    <Link href="/" className="flex items-center gap-2">
+						<Image
+							src="/stories.png"
+							alt="Stories"
+							width={16}
+							height={16}
+							className="w-4 h-4"
+						/>
+						<span>Stories</span>
+                    </Link>
+				</div>
+
+			</div>
+
+			{/* right */}
+			<div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end"> 
+			    <MobileMenu /> 
+			</div>
+
+		</div>
+	)
 }
