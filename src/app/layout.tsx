fd722a3d049af1e4fccabc4275f3a3a24c832d34
int-arsh <jainaakash303@gmail.com>
2025-10-06 23:53:07 +0530

    Add @clerk/nextjs dependency and integrate ClerkProvider in layout. Enhance Navbar with user authentication components and loading states.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,20 @@
 import Navbar from "@/components/Navbar";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html>
-			<body>
-				<div className="w-full bg-white px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-					<Navbar />
-				</div>
-				<div className="bg-slate-100 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-                	{children}
-				</div>
-            </body>
-		</html>
+		<ClerkProvider>
+			<html>
+				<body>
+					<div className="w-full bg-white px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+						<Navbar />
+					</div>
+					<div className="bg-slate-100 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+						{children}
+					</div>
+				</body>
+			</html>
+		</ClerkProvider>
 	);
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import MobileMenu from "./MobileMenu";
 import Image from "next/image";
+import { ClerkLoading, SignedIn, UserButton } from "@clerk/nextjs";
+import { SignedOut } from "@clerk/nextjs";
+import { ClerkLoaded } from "@clerk/nextjs";
 
 export default function Navbar() {
 	return (
@@ -46,11 +49,42 @@ export default function Navbar() {
 						<span>Stories</span>
                     </Link>
 				</div>
-
+				<div className="hidden xl:flex p-2 bg-slate-100 items-center rounded-xl">
+					<input type="text" placeholder="Search..." className="bg-transparent outline-none" />
+					<Image 
+						src="/search.png"
+						alt="Search"
+						width={14}
+						height={14}
+					/>
+				</div>
 			</div>
 
 			{/* right */}
 			<div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end"> 
+				<ClerkLoading>
+					<div className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-gray-500 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white" />
+				</ClerkLoading>
+				<ClerkLoaded>
+					<SignedIn>
+						<div className="cursor-pointer">
+							<Image src="/people.png" alt="" width={24} height={24} />
+						</div>
+						<div className="cursor-pointer">
+							<Image src="/messages.png" alt="" width={20} height={20} />
+						</div>
+						<div className="cursor-pointer">
+						<	Image src="/notifications.png" alt="" width={20} height={20} />
+						</div>
+						<UserButton />
+					</SignedIn>
+					<SignedOut>
+					<div className="flex items-center gap-2 text-sm">
+						<Image src="/login.png" alt="" width={20} height={20} />
+						<Link href="/sign-in">Login/Register</Link>
+					</div>
+					</SignedOut>
+				</ClerkLoaded>
 			    <MobileMenu /> 
 			</div>
 
