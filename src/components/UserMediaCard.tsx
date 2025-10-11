30f2c633a7219bec68c8a0c7fa0046169b93b793
int-arsh <jainaakash303@gmail.com>
2025-10-11 19:18:38 +0530

    Enhance UserInfoCard and UserMediaCard components by adding detailed user information sections, including images and a follow button. Update link text for clarity and improve layout for better user experience.

--- src/components/UserMediaCard.tsx
+++ src/components/UserMediaCard.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const UserMediaCard = ({userId}: {userId: string}) => {
   return (
@@ -6,9 +7,61 @@ const UserMediaCard = ({userId}: {userId: string}) => {
         {/* top */}
         <div className="flex justify-between items-center font-medium">
             <span className="text-gray-500">User Information</span>
-            <Link href="/">View All</Link>
+            <Link href="/" className="text-blue-500 text-xs">
+              See All
+            </Link>
         </div>
         {/* bottom */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <div className="relative w-1/5 h-24">
+            <Image 
+              src="https://images.pexels.com/photos/34036546/pexels-photo-34036546.jpeg" alt="" fill 
+              className="object-cover rounded-md" 
+            />
+          </div>        
+          <div className="relative w-1/5 h-24">
+            <Image 
+              src="https://images.pexels.com/photos/34036546/pexels-photo-34036546.jpeg" alt="Media" fill 
+              className="object-cover rounded-md" 
+            />
+          </div>        
+          <div className="relative w-1/5 h-24">
+            <Image 
+              src="https://images.pexels.com/photos/34036546/pexels-photo-34036546.jpeg" alt="Media" fill 
+              className="object-cover rounded-md" 
+            />
+          </div>        
+          <div className="relative w-1/5 h-24">
+            <Image 
+              src="https://images.pexels.com/photos/34036546/pexels-photo-34036546.jpeg" alt="Media" fill 
+              className="object-cover rounded-md" 
+            />
+          </div>        
+          <div className="relative w-1/5 h-24">
+            <Image 
+              src="https://images.pexels.com/photos/34036546/pexels-photo-34036546.jpeg" alt="Media" fill 
+              className="object-cover rounded-md" 
+            />
+          </div>        
+          <div className="relative w-1/5 h-24">
+            <Image 
+              src="https://images.pexels.com/photos/34036546/pexels-photo-34036546.jpeg" alt="Media" fill 
+              className="object-cover rounded-md" 
+            />
+          </div>        
+          <div className="relative w-1/5 h-24">
+            <Image 
+              src="https://images.pexels.com/photos/34036546/pexels-photo-34036546.jpeg" alt="Media" fill 
+              className="object-cover rounded-md" 
+            />
+          </div>        
+          <div className="relative w-1/5 h-24">
+            <Image 
+              src="https://images.pexels.com/photos/34036546/pexels-photo-34036546.jpeg" alt="Media" fill 
+              className="object-cover rounded-md" 
+            />
+          </div>        
+        </div>
         
     </div>
   )
